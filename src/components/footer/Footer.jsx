c030adc39e31c1dea902850b2aabf49c29b1ee52
komazecnikola <nikola.koma@gmail.com>
2024-03-05 23:07:51 +0100

    footer content and styles added + other stuff

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.css";
 
 function Footer() {
-  return <div></div>;
+  const date = new Date();
+  const year = date.getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footerWrapper">
+          <p>
+            {" "}
+            &copy; {year} React Web Shop by{" "}
+            <a
+              className="footerName"
+              target="_blank"
+              rel="noreferrer"
+              href="https://komazec.dev/"
+            >
+              Nikola Komazec
+            </a>{" "}
+          </p>
+
+          <div className="footerLinks">
+            <Link to={"/privacy-policy"} className="footerLink">
+              Privacy policy
+            </Link>
+            <Link to={"/terms-of-service"} className="footerLink">
+              Terms of service
+            </Link>
+            <Link to={"/contact-us"} className="footerLink">
+              Contact us
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
